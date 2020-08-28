8d06dcef3e726993bd2b3c9d0f5115ba93f3d658
Warisara33 <56625430+Warisara33@users.noreply.github.com>
2020-08-28 23:28:59 +0700

    Update NewFirstapp&finsh

--- app/src/main/java/com/example/newfirstapp/ContactFragment.kt
+++ app/src/main/java/com/example/newfirstapp/ContactFragment.kt
@@ -1,16 +1,52 @@
-package com.example.newfirstapp.contant
+package com.example.newfirstapp.contact
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
-import com.example.newfirstapp.R
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.newfirstapp.database.ContactDatabase
+import com.example.newfirstapp.*
+import com.example.newfirstapp.databinding.FragmentContactBinding
+
 
 class ContactFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var binding = DataBindingUtil.inflate<FragmentContactBinding>(
+            inflater,
+            R.layout.fragment_contact,
+            container,
+            false
+        )
+
+        setHasOptionsMenu(true)
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = ContactDatabase.getInstance(application).contactDao
+        val viewModelFactory = ContactViewModelFactory(dataSource, binding, application)
+        val contactViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
-} 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+}
